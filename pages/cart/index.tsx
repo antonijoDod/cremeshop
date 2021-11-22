37e720867708f2e487575f35fa294bcc1bc47f78
antonijoDod <a.djodj@gmail.com>
2021-11-22 12:35:00 +0000

    Fixed all TS errors

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -15,10 +15,6 @@ const CartPage = () => {
             commerce.cart.update(id, { quantity: quantity }),
     );
 
-    const removeMutation = useMutation((id) => {
-        commerce.cart.remove(id);
-    });
-
     const handleAddProductQuantity = async ({
         id,
         quantity,
@@ -41,11 +37,6 @@ const CartPage = () => {
         refetch();
     };
 
-    const handleRemoveProduct = async (id: string) => {
-        await removeMutation.mutateAsync({ id });
-        refetch();
-    };
-
     if (isLoading) {
         return <div>Loading</div>;
     }
@@ -57,89 +48,94 @@ const CartPage = () => {
                     <div className="w-2/3">
                         <ul>
                             {data?.line_items.length
-                                ? data.line_items.map((item) => (
-                                      <li
-                                          key={item.id}
-                                          className="flex justify-between p-4  border-b"
-                                      >
-                                          <div className="flex">
-                                              <Image
-                                                  src={item.image?.url}
-                                                  alt={item.image?.filename}
-                                                  height={100}
-                                                  width={100}
-                                              />
-                                              <div className="ml-4 flex flex-col justify-between">
-                                                  <h3>{item.name}</h3>
-                                                  <span>Size: </span>
-                                                  <div className="flex items-center">
-                                                      <button
-                                                          className="p-2 bg-gray-200 mr-2"
-                                                          onClick={() =>
-                                                              handleDecreaseProductQuantity(
-                                                                  {
-                                                                      id: item.id,
-                                                                      quantity:
-                                                                          item.quantity -
-                                                                          1,
-                                                                  },
-                                                              )
-                                                          }
-                                                      >
-                                                          -
-                                                      </button>
-                                                      <span className="mr-2">
-                                                          {isRefetching ? (
-                                                              <div className="h-4 w-4 p-2 rounded-full border-8"></div>
-                                                          ) : (
-                                                              item.quantity
-                                                          )}
-                                                      </span>
-                                                      <button
-                                                          className="p-2 bg-gray-200"
-                                                          onClick={() =>
-                                                              handleAddProductQuantity(
-                                                                  {
-                                                                      id: item.id,
-                                                                      quantity:
-                                                                          item.quantity +
-                                                                          1,
-                                                                  },
-                                                              )
-                                                          }
-                                                      >
-                                                          +
-                                                      </button>
+                                ? data.line_items.map(
+                                      ({
+                                          id,
+                                          name,
+                                          image,
+                                          quantity,
+                                          price,
+                                          line_total,
+                                      }) => (
+                                          <li
+                                              key={id}
+                                              className="flex justify-between p-4  border-b"
+                                          >
+                                              <div className="flex">
+                                                  {/*TODO Set backup image when you image is not set in product, fallback image*/}
+                                                  <Image
+                                                      src={
+                                                          image
+                                                              ? image?.url
+                                                              : ""
+                                                      }
+                                                      alt={image?.filename}
+                                                      height={100}
+                                                      width={100}
+                                                  />
+                                                  <div className="ml-4 flex flex-col justify-between">
+                                                      <h3>{name}</h3>
+                                                      <span>Size: </span>
+                                                      <div className="flex items-center">
+                                                          <button
+                                                              className="p-2 bg-gray-200 mr-2"
+                                                              onClick={() =>
+                                                                  handleDecreaseProductQuantity(
+                                                                      {
+                                                                          id: id,
+                                                                          quantity:
+                                                                              quantity -
+                                                                              1,
+                                                                      },
+                                                                  )
+                                                              }
+                                                          >
+                                                              -
+                                                          </button>
+                                                          <span className="mr-2">
+                                                              {isRefetching ? (
+                                                                  <div className="h-4 w-4 p-2 rounded-full border-8"></div>
+                                                              ) : (
+                                                                  quantity
+                                                              )}
+                                                          </span>
+                                                          <button
+                                                              className="p-2 bg-gray-200"
+                                                              onClick={() =>
+                                                                  handleAddProductQuantity(
+                                                                      {
+                                                                          id: id,
+                                                                          quantity:
+                                                                              quantity +
+                                                                              1,
+                                                                      },
+                                                                  )
+                                                              }
+                                                          >
+                                                              +
+                                                          </button>
+                                                      </div>
                                                   </div>
                                               </div>
-                                          </div>
-                                          <div className="flex flex-col text-right justify-between">
-                                              <span>
-                                                  {
-                                                      item.price
-                                                          .formatted_with_code
-                                                  }
-                                              </span>
-                                              <span>
-                                                  Total:{" "}
-                                                  {
-                                                      item.line_total
-                                                          .formatted_with_symbol
-                                                  }
-                                              </span>
-                                              <a
-                                                  className="btn btn-link"
-                                                  onClick={() =>
-                                                      handleRemoveProduct(
-                                                          item.id,
-                                                      )
-                                                  }
-                                              >
-                                                  Remove
-                                              </a>
-                                          </div>
-                                      </li>
-                                  ))
+                                              <div className="flex flex-col text-right justify-between">
+                                                  <span>
+                                                      {
+                                                          price.formatted_with_code
+                                                      }
+                                                  </span>
+                                                  <span>
+                                                      Total:{" "}
+                                                      {
+                                                          line_total.formatted_with_symbol
+                                                      }
+                                                  </span>
+                                                  <a className="btn btn-link">
+                                                      Remove
+                                                  </a>
+                                              </div>
+                                          </li>
+                                      ),
+                                  )
                                 : "Cart is empty"}
                         </ul>
                     </div>
